8384498f95784cbb286b6dec1c795eebcfbc3dd8
spszk <bad14@outlook.it>
2023-09-22 20:48:44 +0200

    1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,58 +1,33 @@
 //exercise 1.4
 
-const Header = (props) => {
-  return (
-    <div>
-      <h2>{props.course}</h2>
-    </div>
-  )
-}
-
-const Content = (arr) => {
-  //console.log(arr);
-  return (
-    <div>
-      <ul>
-        <li>{arr.parts[0].name}: {arr.parts[0].exercises}</li>
-        <li>{arr.parts[1].name}: {arr.parts[1].exercises}</li>
-        <li>{arr.parts[2].name}: {arr.parts[2].exercises}</li>
-      </ul>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  let tot = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
-  return (
-    <div>
-      Total hours: {tot}
-    </div>
-  )
-}
-
-//main
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h2>{course.name}</h2>
+      <ul>
+        <li>{course.parts[0].name}: {course.parts[0].exercises}</li>
+        <li>{course.parts[1].name}: {course.parts[1].exercises}</li>
+        <li>{course.parts[2].name}: {course.parts[2].exercises}</li>
+      </ul>
+      <h4>Total hours: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</h4>
     </div>
   )
 }
