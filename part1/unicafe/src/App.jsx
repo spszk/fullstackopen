a3635670442719d8607e56d828eba875b0a03d87
spszk <bad14@outlook.it>
2023-09-23 15:15:35 +0200

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,18 +15,22 @@ const Button = (props) => {
 }
 
 const Stats = (props) => {
+  const sum = props.bad + props.good + props.neutral
+
   return (
     <div>
       <h2>Statistics</h2>
       <p>Good: {props.good}</p>
       <p>Neutral: {props.neutral}</p>
       <p>Bad: {props.bad}</p>
+      <p>Total: {sum}</p>
+      <p>Average: {sum/3}</p>
+      <p>Positive: {(100 * props.good) / sum}%</p>
     </div>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
