eab0f284901f54a81b4cd8b0f678ff0325b5127e
Cody <bad14@outlook.it>
2024-02-03 11:43:40 +0100

    2.8

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -4,18 +4,21 @@ import Notes from './components/Notes'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0 },
+    { name: 'Arto Hellas', number: '040-1234567', id: 0 },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   // adds person to phonebook
   const addPerson = () => {
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length,
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   // changes newName
@@ -36,15 +39,25 @@ const App = () => {
     else addPerson()
   }
 
+  // changes newNumber
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={checkName}>
-          name: <input
+          <div>name: <input
             placeholder='type...'
             value={newName}
             onChange={handleChange}
-          />
+          /></div>
+          <div>number: <input
+            placeholder='type...'
+            value={newNumber}
+            onChange={handleNumber}
+          /></div>
           <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
--- part2/the-phonebook/src/components/Notes.jsx
+++ part2/the-phonebook/src/components/Notes.jsx
@@ -5,7 +5,7 @@ const Notes = (props) => {
         <ul>
             {persons.map((p) =>
                 <li key={p.id}>
-                    {p.name}
+                    {p.name} {p.number}
                 </li>
             )}
         </ul>
