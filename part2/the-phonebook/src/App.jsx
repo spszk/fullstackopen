544ce0332138769e73799d0426319fe38d93679a
Cody <bad14@outlook.it>
2024-02-03 13:28:56 +0100

    2.9

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -4,16 +4,21 @@ import Notes from './components/Notes'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id: 0 },
+    { name: 'Arto Hellas', number: '040-1234567', same: false, id: 0 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', same: false, id: 1 },
+    { name: 'Dan Abramov', number: '12-43-234345', same: false, id: 2 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', same: false, id: 3 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   // adds person to phonebook
   const addPerson = () => {
     const personObject = {
       name: newName,
       number: newNumber,
+      same: false,
       id: persons.length,
     }
     setPersons(persons.concat(personObject))
@@ -44,24 +49,40 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  // change filterName
+  const handleFilterName = (event) => {
+    setFilterName(event.target.value);
+  }
+  
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      {/* filter */}
+      <div>filter by name: <input
+        type="text"
+        onChange={handleFilterName}
+        />
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={checkName}>
+          {/* add person */}
           <div>name: <input
             placeholder='type...'
             value={newName}
             onChange={handleChange}
           /></div>
+          {/* add number */}
           <div>number: <input
             placeholder='type...'
             value={newNumber}
             onChange={handleNumber}
           /></div>
+          {/* submit */}
           <button type="submit">add</button>
       </form>
+      {/* show list */}
       <h2>Numbers</h2>
-      <Notes persons={persons} />
+      <Notes persons={persons} filter={filterName} />
     </div>
   )
 }
--- part2/the-phonebook/src/components/Notes.jsx
+++ part2/the-phonebook/src/components/Notes.jsx
@@ -1,12 +1,26 @@
 const Notes = (props) => {
-    const {persons} = props
+    const persons = props.persons
+    const filter = props.filter
+    // console.log((persons.name).slice(0,3));
     return (
-        // <div>debug</div>
         <ul>
-            {persons.map((p) =>
-                <li key={p.id}>
-                    {p.name} {p.number}
-                </li>
+            {persons.map((p) => {
+                const currentName = p.name
+                // console.log(typeof(currentName));
+                let match = currentName.slice(0, filter.length)
+                if (filter === "" || filter === " ") {
+                    return (
+                        <li key={p.id}>
+                            {p.name} {p.number}
+                        </li>
+                    )
+                } else if (filter === match) {
+                    return (
+                        <li key={p.id}>
+                        {p.name} {p.number}
+                        </li>
+                    )}
+                }
             )}
         </ul>
     )
